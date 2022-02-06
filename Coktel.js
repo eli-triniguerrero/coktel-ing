36e15705fb762e6783021b169be9e6c5b8d65572
Trinity <elizabethtrinitygs@gmail.com>
2022-02-06 00:13:06 -0600

    creando templates principales

---
+++ Coktel.js
@@ -0,0 +1,18 @@
+const Coktel = () => {
+    const view = `
+    <div class="coktels-inner">
+        <article class="coktels-card">
+            <img src="image" alt="name" />
+            <h2>Name</h2>
+        </article>
+        <article class="coktels-card">
+            <h3>Ingredients:</h3>
+            <h3>Instructions:</h3>
+            <h3>Glass:</h3>
+        </article>
+    </div>
+    `;
+    return view;
+}
+
+export default Coktel;
