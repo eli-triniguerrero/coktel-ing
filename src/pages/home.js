d275027eacc867c57742d964977e276006ddf72e
Trinity <elizabethtrinitygs@gmail.com>
2022-02-06 00:00:52 -0600

    creando primer template

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+// retorna la vista 
+const Home = () => {
+    const view = `
+    <div class="coktels">
+        <article class="coktel-item">
+            <a href="#1">
+                <img src="image" alt="name" />
+                <h2>Name</h2>
+            </a>
+        </article>
+    </div>
+    
+    `;
+    return view;
+};
+
+export default Home;
